12fb4ff475f69a04d3f410d54e81499ae52389f4
alex <alex.tran234@gmail.com>
2018-03-04 07:29:03 +1100

    add test for triggerCooldown

--- src/ZombieFeeding.t.sol
+++ src/ZombieFeeding.t.sol
@@ -4,11 +4,25 @@ import "ds-test/test.sol";
 
 import "./ZombieFeeding.sol";
 
+
+contract ZombieFeedingInherited is ZombieFeeding {
+	
+	function triggerCooldown(uint _zombieId) public {
+		Zombie storage zombie = zombies[_zombieId];
+		_triggerCooldown(zombie);
+	}
+	
+	function getReadyTime(uint _zombieId) public returns(uint32) {
+		Zombie storage zombie = zombies[_zombieId];
+		return zombie.readyTime;
+	}
+}
+
 contract ZombieFeedingTest is DSTest, ZombieFactoryEvents {
-	ZombieFeeding zombieFeeding;
+	ZombieFeedingInherited  zombieFeeding;
     
 	function setUp() public {
-		zombieFeeding = new ZombieFeeding();
+		zombieFeeding = new ZombieFeedingInherited();
 	}
     
 	function test_feedAndMultiply() public {
@@ -27,5 +41,14 @@ contract ZombieFeedingTest is DSTest, ZombieFactoryEvents {
 		
 		zombieFeeding.feedAndMultiply(0, 1, "nonKitty");        
 		NewZombie(1, "NoName", zombieFeeding.getDnaByIndex(2));
-    }
+	}
+	
+	function test_triggerCooldown() public {
+		zombieFeeding.createRandomZombie("a");
+		uint32 expectedReadyTimeAfter = uint32(now + 1 days);
+		zombieFeeding.triggerCooldown(0);
+		uint32 readyTimeAfter = zombieFeeding.getReadyTime(0);
+ 
+		assert(readyTimeAfter == expectedReadyTimeAfter );
+	}
 }
