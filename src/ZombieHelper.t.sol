dc7776a4b093477ac215e9f29cf78e90c755d1b5
alex <alex.tran234@gmail.com>
2018-03-03 15:28:49 +1100

    add levelUp logic

--- src/ZombieHelper.t.sol
+++ src/ZombieHelper.t.sol
@@ -10,5 +10,18 @@ contract ZombieHelperTest is DSTest {
 	function setUp() public {
 		zombieHelper = new ZombieHelper();
 	}
+
+	function test_levelUp() public {
+		zombieHelper.createRandomZombie("a");
+		zombieHelper.levelUp.value(0.001 ether)(0);
+		// TODO : need to get the actual level to verify?
+	}	
 	
+	function testFail_levelUpWrongAmount() public {
+		zombieHelper.createRandomZombie("a");
+		
+		// not the correct amount of ether sent 
+		// so should throw
+		zombieHelper.levelUp.value(0.002 ether)(0);
+	}
 }
